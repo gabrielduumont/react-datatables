eebecdef8efdb84e7b32e53a9067c0ac4fdc1453
Gabriel Dumont <gabrielduumont@gmail.com>
2020-09-23 11:35:57 -0300

    Updates

---
+++ precache-manifest.072d9883eb941908f26e76f30e42dd89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d930ea465b54e738fd8fb25630cc84bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04dbbe6498961fd9aa87",
+    "url": "/static/css/2.2865438f.chunk.css"
+  },
+  {
+    "revision": "3da1c83700b7b78d41bd",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "04dbbe6498961fd9aa87",
+    "url": "/static/js/2.a3eb0bda.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.a3eb0bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da1c83700b7b78d41bd",
+    "url": "/static/js/main.05ede5a3.chunk.js"
+  },
+  {
+    "revision": "bc51d2a1e6b2c6d65a5b",
+    "url": "/static/js/runtime-main.1bd49777.js"
+  }
+]);
